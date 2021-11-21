f3d11b751024035c18f28ba73f0a7638302a5d22
Shayeque Ahmed <shayequeahmed16@gmail.com>
2021-11-21 21:22:04 +0530

    Payment System with success and Error Message

--- app/Controllers/PaymentController.php
+++ app/Controllers/PaymentController.php
@@ -3,9 +3,15 @@
 namespace App\Controllers;
 
 use App\Config;
+use Razorpay\Api\Api;
 use Fantom\Controller;
+use App\Support\Response;
 use App\Middlewares\UserAuthMiddleware;
+use App\EComm\Validators\PaymentValidator;
 use App\EComm\Repositories\OrderRepository;
+use App\EComm\Repositories\PaymentRepository;
+use Razorpay\Api\Errors\SignatureVerificationError;
+
 
 /**
  * PaymentController
@@ -33,6 +39,66 @@ class PaymentController extends Controller
 		$this->view->render("Payment/create.php", [
 			'rzp_data' => $data,
 		]);
+	}
+
+	protected function store()
+	{
+		$res = new Response();
+		// @TODO Validate
+		$v = new PaymentValidator();
+		$v->validatePaymentCreate();
+		if ($v->hasError()) {
+			$res->setErrors($v->validationErrors()->all());
+			return $res->send();
+		}
+
+		$order = OrderRepository::byRazorypayOrderId($_POST['razorpay_order_id']);
+		$_POST['order_id'] = $order->id;
+		$_POST['user_id'] = $order->user_id;
+		$payment = PaymentRepository::make($_POST);
+		$razorpay = new Api(Config::get('rzp_key'), Config::get('rzp_secret'));
+		$attributes = [
+			'razorpay_order_id' 		=> $order->rzp_order_id,
+			'razorpay_payment_id' 		=> $_POST['razorpay_payment_id'],
+			'razorpay_signature' 		=> $_POST['razorpay_signature'],
+		];
+		try {
+			$razorpay->utility->verifyPaymentSignature($attributes);
+		} catch (SignatureVerificationError $e) {
+			$res->setErrors(['payment' => 'Payment signature verification failed']);
+			$payment->status = PaymentRepository::STATUS_FAILURE;
+		}
+
+		if ($payment->save() === false) {
+			$res->setErrors(['payment' => 'Failed to save payment']);
+			return $res->send();
+		}
+
+		return $res->send();
+	}
+
+	protected function failure()
+	{
+		$res = new Response();
+		// @TODO Validate
+		$v = new PaymentValidator();
+		$v->validatePaymentCreate();
+		if ($v->hasError()) {
+			$res->setErrors($v->validationErrors()->all());
+			return $res->send();
+		}
+
+		$order = OrderRepository::byRazorypayOrderId($_POST['razorpay_order_id']);
+		$_POST['user_id'] = $order->user_id;
+		$payment = PaymentRepository::make($_POST);
+		$payment->status = PaymentRepository::STATUS_FAILURE;
+		if ($payment->save() === false) {
+			// 
+			$res->setErrors(['payment' => 'Failed to save payment']);
+			return $res->send();
+		}
+
+		return $res->send();
 	}
 
 	protected function before()
--- app/Views/Payment/create.php
+++ app/Views/Payment/create.php
@@ -2,23 +2,42 @@
 
 <button id="rzp-button1">Pay</button>
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
 var options = <?= json_encode($rzp_data) ?>;
 options.handler = (response) => {
-	alert(response.razorpay_payment_id);
-    alert(response.razorpay_order_id);
-    alert(response.razorpay_signature);
+    // alert(response.razorpay_payment_id);
+    // alert(response.razorpay_order_id);
+    // alert(response.razorpay_signature);
+    const url = "/payment/store";
+    const form = new URLSearchParams();
+    form.append("razorpay_payment_id", response.razorpay_payment_id);
+    form.append("razorpay_order_id", response.razorpay_order_id);
+    form.append("razorpay_signature", response.razorpay_signature);
+
+    axios.post(url, form).then((response) => {
+        alert("Payment Susccessful!");
+        console.log(response);
+    }).catch((error) => {
+        alert("Payment Failure!");
+        console.error(error);
+    });
 };
 
 var rzp1 = new Razorpay(options);
 rzp1.on('payment.failed', function (response){
-        alert(response.error.code);
-        alert(response.error.description);
-        alert(response.error.source);
-        alert(response.error.step);
-        alert(response.error.reason);
-        alert(response.error.metadata.order_id);
-        alert(response.error.metadata.payment_id);
+    const url = "/payment/failure";
+    const form = new URLSearchParams();
+    form.append("razorpay_payment_id", response.error.metadata.payment_id);
+    form.append("razorpay_order_id", response.error.metadata.order_id);
+
+    axios.post(url, form).then((response) => {
+        // alert("Payment Susccessful!");
+    }).catch((error) => {
+        // alert("Payment Failure!");
+    });
+
+    console.error(response.error);
 });
 document.getElementById('rzp-button1').onclick = function(e){
     rzp1.open();
