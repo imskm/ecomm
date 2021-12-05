23a158f53b5683fe99a54db029fbd2d3382047af
Shayeque Ahmed <shayequeahmed16@gmail.com>
2021-12-05 15:59:11 +0530

    WIP Order Detail Page in Admin

---
+++ app/Controllers/Admin/OrderController.php
@@ -0,0 +1,42 @@
+<?php 
+namespace App\Controllers\Admin;
+use Fantom\Controller;
+use App\Middlewares\AdminAuthMiddleware;
+use App\EComm\Repositories\OrderRepository;
+use App\EComm\Repositories\OrderItemRepository;
+
+/**
+ * 
+ */
+class OrderController extends Controller
+{
+	
+	protected function index()
+	{
+		$orders = OrderRepository::recent(get_page())->get();
+		$this->view->render('Admin/Order/index.php',[
+			"orders" =>$orders,
+		]);
+	}
+
+	protected function show()
+	{
+		$order_id = (int) $this->route_params['id'];
+		$order = OrderRepository::find($order_id);
+		if (is_null($order)) {
+			Session::flash("error", "Order id not found");
+			redirect("admin/order/index");
+		}
+		$items = $order->orderItems()->get();
+
+		echo "<pre>";
+		print_r($items);
+		echo "</pre>";
+
+	}
+
+	protected function before()
+	{
+		return (new AdminAuthMiddleware)();
+	}
+}
